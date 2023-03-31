7e8885465fa54d473677567d338cb96d31abc72f
GuillaumeDORIA <guillaume.doria@edu.devinci.fr>
2023-03-31 18:50:29 +0200

    Update dedicatedbrand.js

--- server/eshops/dedicatedbrand.js
+++ server/eshops/dedicatedbrand.js
@@ -1,45 +1,44 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-/**
- * Parse webpage e-shop
- * @param  {String} data - html response
- * @return {Array} products
- */
-const parse = data => {
+const BASE_URL = 'https://www.dedicatedbrand.com/en/men/all-men#page=';
+
+const parseData = (data) => {
   const $ = cheerio.load(data);
 
   return $('.productList-container .productList')
     .map((i, element) => {
+      const brand = "Dedicated";
       const name = $(element)
         .find('.productList-title')
         .text()
         .trim()
         .replace(/\s/g, ' ');
-      const price = parseInt(
+      const title = $(element)
+        .find('.productList-title')
+        .text()
+        .trim();
+      const color = title.split(' ').pop();
+      const price = parseFloat(
         $(element)
           .find('.productList-price')
           .text()
       );
 
-      return {name, price};
+      return { brand, name, price, color };
     })
     .get();
 };
 
-/**
- * Scrape all the products for a given url page
- * @param  {[type]}  url
- * @return {Array|null}
- */
-module.exports.scrape = async url => {
+const fetchData = async (url) => {
   try {
     const response = await fetch(url);
 
     if (response.ok) {
       const body = await response.text();
 
-      return parse(body);
+      return parseData(body);
     }
 
     console.error(response);
@@ -50,3 +49,39 @@ module.exports.scrape = async url => {
     return null;
   }
 };
+
+const scrapePages = async (startPage, endPage) => {
+  const allProducts = [];
+
+  for (let i = startPage; i <= endPage; i++) {
+    const url = BASE_URL + i;
+    console.log(`🕵️‍♀️  browsing ${url}`);
+
+    const products = await fetchData(url);
+
+    if (products && products.length > 0) {
+      allProducts.push(...products);
+    } else {
+      break;
+    }
+  }
+
+  return allProducts;
+};
+
+const writeDataToFile = async () => {
+  try {
+    const scrapedData = await scrapePages(1, 17);
+    console.log(scrapedData);
+
+    const jsonData = JSON.stringify(scrapedData);
+    fs.writeFile('dedicated_products.json', jsonData, (err) => {
+      if (err) throw err;
+      console.log('Data written to file-------');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+writeDataToFile();
